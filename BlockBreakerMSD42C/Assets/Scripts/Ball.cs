28154059f17b2da857008d917e08f58b74820e8f
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-10-21 16:09:33 +0200

    Connected the Paddle to the Mouse and the Ball to the Paddle
    Connected the Paddle to the Mouse and the Ball to the Paddle

---
+++ BlockBreakerMSD42C/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle myPaddle;
+
+    Vector2 paddleToBallDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = this.transform.position - myPaddle.transform.position;
+            
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePosition = myPaddle.transform.position;
+
+        //set the Ball Position = Paddle Position + the Distance
+        this.transform.position = paddlePosition + paddleToBallDistance;
+
+    }
+}

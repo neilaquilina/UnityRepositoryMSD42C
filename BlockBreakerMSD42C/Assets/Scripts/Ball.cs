812e36e67f0271c5ccc2e54883117fd6af4b5c93
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-10-23 11:37:38 +0200

    Launch Ball On Clcik, Set the Play Area

--- BlockBreakerMSD42C/Assets/Scripts/Ball.cs
+++ BlockBreakerMSD42C/Assets/Scripts/Ball.cs
@@ -8,6 +8,8 @@ public class Ball : MonoBehaviour
 
     Vector2 paddleToBallDistance;
 
+    bool hasStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,30 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 paddlePosition = myPaddle.transform.position;
+        if (!hasStarted) // hasStarted == false
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
 
+
+
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if (Input.GetMouseButtonDown(0)) //left Click
+        {
+            hasStarted = true;
+            //shoot the ball
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = myPaddle.transform.position;
         //set the Ball Position = Paddle Position + the Distance
         this.transform.position = paddlePosition + paddleToBallDistance;
-
     }
 }

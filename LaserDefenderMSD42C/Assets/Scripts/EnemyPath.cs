f48e8e7738b1b6eb4a2f379169eea51b2b63b790
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-20 11:39:32 +0100

    Set Boundaries and started the Waypoints and Path

---
+++ LaserDefenderMSD42C/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] List <Transform> waypoints;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- LaserDefenderMSD42C/Assets/Scripts/Player.cs
+++ LaserDefenderMSD42C/Assets/Scripts/Player.cs
@@ -7,16 +7,33 @@ public class Player : MonoBehaviour
 
     [SerializeField] float movementSpeed = 10f;
 
+    float xMin, xMax, yMin, yMax;
+
+    float padding = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+
+        Camera gameCamera = Camera.main;
+        //save the minimum and max x-axis of the camera in xMin and xMax
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
     }
 
     private float MoveX()
@@ -41,6 +58,9 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        transform.position = new Vector2(MoveX(), MoveY());
+        float xPos = Mathf.Clamp(MoveX(), xMin, xMax);
+        float yPos = Mathf.Clamp(MoveY(), yMin, yMax);
+
+        transform.position = new Vector2(xPos, yPos);
 ;    }
 }

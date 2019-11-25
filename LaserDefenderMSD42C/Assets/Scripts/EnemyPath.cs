15f434924a571b365b1449922706581843003b18
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-25 16:21:16 +0100

    updated the enemy to move on a path

--- LaserDefenderMSD42C/Assets/Scripts/EnemyPath.cs
+++ LaserDefenderMSD42C/Assets/Scripts/EnemyPath.cs
@@ -6,6 +6,8 @@ public class EnemyPath : MonoBehaviour
 {
     [SerializeField] List <Transform> waypoints;
 
+    float enemySpeed = 2f;
+
     int waypointIndex = 0;
 
     // Start is called before the first frame update
@@ -17,6 +19,35 @@ public class EnemyPath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMovement();
+    }
+
+    private void EnemyMovement()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            //set targetPosition to the next waypoint
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+
+            float movementThisFrame = enemySpeed * Time.deltaTime;
+            
+            //move the enemy ship from the current position towards the targePosition at the given movement speed
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            //if we reached the waypoint
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+
+        }
+        //if enemy reaches the last waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }

038e056c8ff3f6514485e8ef344cb3759b6a046c
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-28 11:48:19 +0100

    Created coroutine to make enemies spawn and shoot

---
+++ LaserDefenderMSD42C/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    [SerializeField] GameObject enemyLaser;
+    [SerializeField] float laserSpeed = -10f;
+
+    float laserFiringTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyFire());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EnemyFire()
+    {
+        //while coroutine is running
+        while(true)
+        {
+            GameObject laser = Instantiate(enemyLaser, transform.position, transform.rotation);
+
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+
+            laserFiringTime = Random.Range(0.5f, 1.0f);
+
+            yield return new WaitForSeconds(laserFiringTime);
+        }
+    }
+
+    
+}
---
+++ LaserDefenderMSD42C/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform firstSpawnPoint;
+    [SerializeField] int maximumEnemies = 10;
+    [SerializeField] float spawnTime = 1.5f;
+
+    int currentEnemies = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //coroutine to spawn an enemy (max 10) every 1.5 seconds
+    IEnumerator SpawnEnemies()
+    {
+        while(currentEnemies <= maximumEnemies)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, firstSpawnPoint.transform.position, transform.rotation);
+            currentEnemies++;
+
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
